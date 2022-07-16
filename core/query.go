04acd0a31f79060f68fc2bb3ab1b7cdf3984f974
Shubh Karman <sksingh2211@gmail.com>
2022-07-16 22:39:39 +0530

    Added Query Interface for future use

--- core/query.go
+++ core/query.go
@@ -1,13 +1,14 @@
 package core
 
 import(
+	"fmt"
 	"github.com/qascade/yast/scraper"
 )
-
-// type Query interface {
-// 	Search()
-// 	//GetResults() 
-// }
+//Keeping this here for now, might be used in building Query Registry
+type Query interface {
+	Search()
+	GetResults() 
+}
 
 type SearchQuery struct{
 	Searched bool //To check whether the query has been searched or not
@@ -25,5 +26,17 @@ func NewSearchQuery(context *scraper.QueryContext) *SearchQuery{
 func (q *SearchQuery) Search() (results []*scraper.Result, err error){
 	scraper := scraper.NewScraper()
 	results, err = scraper.Scrape(q.Context)
-	return
+	if err != nil {
+		return nil, err
+	}
+	q.Results = results
+	q.Searched = true
+	return	
+}
+func (q *SearchQuery) GetResults() (results []*scraper.Result, err error){
+	if !q.Searched {
+		err = fmt.Errorf("Query has not been searched yet")
+		return
+	}
+	return q.Results, nil
 }
