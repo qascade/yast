d1c222418c36cab0945274d0d79e9767a8394174
Shubh Karman <sksingh2211@gmail.com>
2022-08-06 00:39:42 +0530

    added config.go

--- core/config.go
+++ core/config.go
@@ -6,10 +6,18 @@ import(
 
 	"github.com/qascade/yast/tui"
 	"github.com/qascade/yast/utils"
+	"github.com/tidwall/sjson"
 )
 
 //This function will call interactive tui for taking input of user preferences. 
 func FillConfigJSON(configFile *os.File) error {
 	utils.LogUnimplementedFunc()
 	return nil
+}
+
+
+type ConfigBuildSpec struct {
+	Player string `json:"player"`
+	TargetPreference string `json:"target-preference"`
+	QueryHistory bool `json:"query-history"`
 }
